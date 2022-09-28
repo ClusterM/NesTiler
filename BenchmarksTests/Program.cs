610332bc0ff089224747cf35bdb51cefc4e29eac
Alexey 'Cluster' Avdyukhin <clusterrr@clusterrr.com>
2022-09-29 01:02:22 +0400

    Minor refactoring

--- BenchmarksTests/Program.cs
+++ BenchmarksTests/Program.cs
@@ -29,139 +29,144 @@ namespace com.clusterrr.Famicom.NesTiler.Benchmarks
         public void BenchmarkBelayaAkula()
         {
             var imagePath = @"Images\belaya_akula.gif";
-            DoBenchmark(imagePath);
+            DoBenchmarkSplit4(imagePath);
         }
 
         [Benchmark]
         public void BenchmarkBuhanka()
         {
             var imagePath = @"Images\buhanka.gif";
-            DoBenchmark(imagePath);
+            DoBenchmarkSplit4(imagePath);
         }
 
         [Benchmark]
         public void BenchmarkChernobyl()
         {
             var imagePath = @"Images\chernobyl.gif";
-            DoBenchmark(imagePath);
+            DoBenchmarkSplit4(imagePath);
         }
 
         [Benchmark]
         public void BenchmarkDira()
         {
             var imagePath = @"Images\dira.gif";
-            DoBenchmark(imagePath);
+            DoBenchmarkSplit4(imagePath);
         }
 
         [Benchmark]
         public void BenchmarkGlaza()
         {
             var imagePath = @"Images\glaza.gif";
-            DoBenchmark(imagePath);
+            DoBenchmarkSplit4(imagePath);
         }
 
         [Benchmark]
         public void BenchmarkGorgona()
         {
             var imagePath = @"Images\gorgona.gif";
-            DoBenchmark(imagePath);
+            DoBenchmarkSplit4(imagePath);
         }
 
         [Benchmark]
         public void BenchmarkMyatejl()
         {
             var imagePath = @"Images\myatej.gif";
-            DoBenchmark(imagePath);
+            DoBenchmarkSplit4(imagePath);
         }
 
         [Benchmark]
         public void BenchmarkPagoda()
         {
             var imagePath = @"Images\pagoda.gif";
-            DoBenchmark(imagePath);
+            DoBenchmarkSplit4(imagePath);
         }
 
         [Benchmark]
         public void BenchmarkRayon4()
         {
             var imagePath = @"Images\rayon4.gif";
-            DoBenchmark(imagePath);
+            DoBenchmarkSplit4(imagePath);
         }
 
         [Benchmark]
         public void BenchmarkShkola()
         {
             var imagePath = @"Images\shkola.gif";
-            DoBenchmark(imagePath);
+            DoBenchmarkSplit4(imagePath);
         }
 
         [Benchmark]
         public void BenchmarkSindikat()
         {
             var imagePath = @"Images\sindikat.gif";
-            DoBenchmark(imagePath);
+            DoBenchmarkSplit4(imagePath);
         }
 
         [Benchmark]
         public void BenchmarkSputnik()
         {
             var imagePath = @"Images\sputnik.gif";
-            DoBenchmark(imagePath);
+            DoBenchmarkSplit4(imagePath);
         }
 
         [Benchmark]
         public void BenchmarkSworm()
         {
             var imagePath = @"Images\sworm.gif";
-            DoBenchmark(imagePath);
+            DoBenchmarkSplit4(imagePath);
         }
 
         [Benchmark]
         public void BenchmarkTrailerPark()
         {
             var imagePath = @"Images\trailer-park.gif";
-            DoBenchmark(imagePath);
+            DoBenchmarkSplit4(imagePath);
         }
 
         [Benchmark]
         public void BenchmarkWarfaceLogo()
         {
             var imagePath = @"Images\warface_logo.gif";
-            DoBenchmark(imagePath);
+            DoBenchmarkSplit4(imagePath);
         }
 
         [Benchmark]
         public void BenchmarkZapravka()
         {
             var imagePath = @"Images\zapravka.gif";
-            DoBenchmark(imagePath);
+            DoBenchmarkSplit4(imagePath);
         }
 
-        public void DoBenchmark(string imagePath)
+        private string PatternTablePath(string prefix, int number) => $"{prefix}_pattern_{number}.bin";
+        private string NameTablePath(string prefix, int number) => $"{prefix}_name_table_{number}.bin";
+        private string AttrTablePath(string prefix, int number) => $"{prefix}_attr_table_{number}.bin";
+        private string PalettePath(string prefix, int number) => $"{prefix}_palette_{number}.bin";
+
+        public void DoBenchmarkSplit4(string imagePath)
         {
-            var suffix = Path.GetFileNameWithoutExtension(imagePath);
+            var prefix = Path.GetFileName(imagePath);
             var args = new string[] {
                 "--enable-palettes", "0,1,2,3",
                 "-i0", $"{imagePath}:0:64",
                 "-i1", $"{imagePath}:64:64",
                 "-i2", $"{imagePath}:128:64",
                 "-i3", $"{imagePath}:192:48",
-                "--out-pattern-table0", $"{suffix}-pattern-table-0.bin",
-                "--out-pattern-table1", $"{suffix}-pattern-table-1.bin",
-                "--out-pattern-table2", $"{suffix}-pattern-table-2.bin",
-                "--out-pattern-table3", $"{suffix}-pattern-table-3.bin",
-                "--out-name-table0", $"{suffix}-name-table-0.bin",
-                "--out-name-table1", $"{suffix}-name-table-1.bin",
-                "--out-name-table2", $"{suffix}-name-table-2.bin",
-                "--out-name-table3", $"{suffix}-name-table-3.bin",
-                "--out-attribute-table0", $"{suffix}-attribute-table-0.bin",
-                "--out-attribute-table1", $"{suffix}-attribute-table-1.bin",
-                "--out-attribute-table2", $"{suffix}-attribute-table-2.bin",
-                "--out-attribute-table3", $"{suffix}-attribute-table-3.bin",
-                "--out-palette0", $"{suffix}-palette-0.bin",
-                "--out-palette1", $"{suffix}-palette-1.bin",
-                "--out-palette2", $"{suffix}-palette-2.bin",
-                "--out-palette3", $"{suffix}-palette-3.bin"
+                "--out-pattern-table0", PatternTablePath(prefix, 0),
+                "--out-pattern-table1", PatternTablePath(prefix, 1),
+                "--out-pattern-table2", PatternTablePath(prefix, 2),
+                "--out-pattern-table3", PatternTablePath(prefix, 3),
+                "--out-name-table0", NameTablePath(prefix, 0),
+                "--out-name-table1", NameTablePath(prefix, 1),
+                "--out-name-table2", NameTablePath(prefix, 2),
+                "--out-name-table3", NameTablePath(prefix, 3),
+                "--out-attribute-table0", AttrTablePath(prefix, 0),
+                "--out-attribute-table1", AttrTablePath(prefix, 1),
+                "--out-attribute-table2", AttrTablePath(prefix, 2),
+                "--out-attribute-table3", AttrTablePath(prefix, 3),
+                "--out-palette0", PalettePath(prefix, 0),
+                "--out-palette1", PalettePath(prefix, 1),
+                "--out-palette2", PalettePath(prefix, 2),
+                "--out-palette3", PalettePath(prefix, 3),
             };
             var r = Program.Main(args);
             if (r != 0) throw new InvalidOperationException($"Return code: {r}");
